875bfd83276cb10b7a56d44dea123a98f36d80c0
yashvarma47 <yashovarma47@gmail.com>
2024-05-13 20:21:29 +0530

    New Components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { WorkComponent } from './work/work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutmeComponent,
+    ContactComponent,
+    ServicesComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
